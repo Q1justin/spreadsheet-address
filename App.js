004b6373842f3b1513e7bdc16599f11bdd5d8e54
Gyuwon (Justin) Do <q1justindo@gmail.com>
2023-04-11 17:02:36 -0400

    Limit document picker to spreadsheets

--- App.js
+++ App.js
@@ -6,12 +6,33 @@ import * as DocumentPicker from 'expo-document-picker';
 
 export default function App() {
 
-  const [fileResponse, setFileResponse] = useState([]);
+  const [selectedDocument, setSelectedDocument] = useState({});
 
   const getAddress = async () => {
     try {
-      let result = await DocumentPicker.getDocumentAsync({});
+      let result = await DocumentPicker.getDocumentAsync({
+        multiple: false,
+        type: [
+          'application/vnd.ms-excel',
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          'text/comma-separated-values',
+          'text/csv'
+        ]
+      });
+      if (result.type === 'success') {
+        setSelectedDocument(result)
+      }
       console.log(result);
+      // Object {
+        //CSV
+      //   "mimeType": "text/comma-separated-values",
+      //xlsx
+      // "mimeType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      //   "name": "RDT_20230411_0938231034377197161664754.jpg",
+      //   "size": 814571,
+      //   "type": "success",
+      //   "uri": "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252Fspreadsheet-address-2c033473-0ae5-405e-b194-6a835d1ac9ec/DocumentPicker/7313749b-04d9-45d3-a8d4-c7a8654aac20.jpg",
+      // }
       setFileResponse([]);
     }
     catch(e) {
